9e8964059cd409173227356f19984303ba882b37
Yoonji Oh <yoonjee36@duksung.ac.kr>
2022-08-21 23:26:14 +0900

    합치기 완료

--- com_project/diary/views.py
+++ com_project/diary/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, UpdateView
 
-def index(request):
-    return render(
-        request,
-        'diary/index.html',
-    )
+from . import models
+from .models import Post
+from django.core.paginator import Paginator
+
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    template_name = 'diary/post_list.html'
+    paginate_by = 9
+
+
+#def post_list(request):
+#    posts_all = Post.objects.all()
+#    paginator = Paginator(posts_all, 9)
+#    page = request.GET.get('page')
+#    posts = paginator.get_page(page)
+
+#    return render(request, 'post_list.html', {'posts': posts})
+
+
+class PostDetail(DetailView):
+    model = Post
+
+
+class PostUpdate(UpdateView):
+    model = Post
+    fields = ['content']
+
+    template_name = 'diary/post_update_form.html'
