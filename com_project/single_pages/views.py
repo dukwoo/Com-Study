c60c669b4d9a156864d102bd8a504e4d2b116a42
Sooin <changin66@naver.com>
2022-08-21 22:54:00 +0900

    gif 배경이미지 삽입

--- com_project/single_pages/views.py
+++ com_project/single_pages/views.py
@@ -10,23 +10,14 @@ def today_word(request):
     for comment in comments:
         count += 1
         
-        
     #랜덤 글귀 뜨도록 설정
     comment_001 = Comment.objects.get(id=random.randint(1,count))
-    comment_002 = Comment.objects.get(id=random.randint(1,count))
-    comment_003 = Comment.objects.get(id=random.randint(1,count))
-    comment_004 = Comment.objects.get(id=random.randint(1,count))
-    comment_005 = Comment.objects.get(id=random.randint(1,count))
 
     return render(
         request,
         'single_pages/comment_list.html',
         {
             'comment_001': comment_001,
-            'comment_002': comment_002,
-            'comment_003': comment_003,
-            'comment_004': comment_004,
-            'comment_005': comment_005,
         }
     )
 
